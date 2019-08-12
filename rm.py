e3e4dc6c024f4fe448ac02463d83afd545529317
Mani Jamali <54019002+wet-kernel@users.noreply.github.com>
2019-08-12 12:18:54 +0430

    Delete rm.py

--- rm.py
+++
@@ -1,8 +0,0 @@
-def rm (values):
-    for i in values:
-        if os.path.isfile(root+"/"+i):
-            os.remove (root+"/"+i)
-        elif os.path.isdir (root+"/"+i):
-            print(process_colors.FAIL + i + ": isn't a file." + process_colors.ENDC)
-        else:
-            print(process_colors.FAIL + i + ": file not found." + process_colors.ENDC)
