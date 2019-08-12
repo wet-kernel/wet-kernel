b41e237211a0c6a0751e5029baccae2660b0f24e
Mani Jamali <54019002+wet-kernel@users.noreply.github.com>
2019-08-12 12:19:41 +0430

    Delete out.py

--- out.py
+++
@@ -1,4 +0,0 @@
-
-def out(values):
-    for i in values:
-        print(i.replace("\\n","\n").replace("\\a","\a").replace("\\b","\b").replace("\\f","\f").replace("\\r","\r").replace("\\t","\t").replace("\\v","\v").replace("\\s"," "),end=' ')
