e89ab622c0c239159bd96a83f65c8210553f35f7
Mani Jamali <54019002+wet-kernel@users.noreply.github.com>
2019-08-12 14:50:04 +0430

    Update hostname.py

--- include/hostname.py
+++ include/hostname.py
@@ -1,6 +1,6 @@
 def get_hostname():
     hostname_filename = "etc/hostname"
     file = open (hostname_filename,"r")
-    strv = file.read()
+    strv = file.readline()
     file.close()
     return strv
