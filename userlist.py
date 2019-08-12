eb1f9a965b39f19706ef0dacb2018b15e98111fb
Mani Jamali <54019002+wet-kernel@users.noreply.github.com>
2019-08-12 12:17:06 +0430

    Delete userlist.py

--- userlist.py
+++
@@ -1,8 +0,0 @@
-def userlist ():
-    users = os.listdir("desk/")
-    for user in users:
-        if os.path.isfile ("etc/users/"+user):
-            print (process_colors.BOLD+"    "+user+process_colors.ENDC+"\tActive")
-        else:
-            print (process_colors.BOLD+process_colors.FAIL+"    "+user+process_colors.ENDC+"    Inactive")
-    print(process_colors.BOLD + "    root"+process_colors.ENDC + "\tActive")
