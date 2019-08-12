3bf9b8b65ae610ceca767bb266352e28251df161
Mani Jamali <54019002+wet-kernel@users.noreply.github.com>
2019-08-12 12:21:08 +0430

    Delete exec.py

--- exec.py
+++
@@ -1,8 +0,0 @@
-def exec (filename,args):
-    if os.path.isfile(root + "/bin/" + filename + ".pyc"):
-        process.clean_switch()
-        copyfile(root + "/bin/"+ filename + ".pyc", "var/switch_process.pyc")
-        from var import switch_process
-        switch_process = reload(switch_process)
-        switch_process.main(args)
-        process.clean_switch()
